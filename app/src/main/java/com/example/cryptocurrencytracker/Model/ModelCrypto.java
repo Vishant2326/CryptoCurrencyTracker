769b7fcd0dc6d079e1363843eaf3a6a754633ab0
Vishant Patil <vishantpatil6421@gmail.com>
2022-07-14 14:50:04 +0530

    Initial commit

---
+++ app/src/main/java/com/example/cryptocurrencytracker/Model/ModelCrypto.java
@@ -0,0 +1,51 @@
+package com.example.cryptocurrencytracker.Model;
+
+import androidx.annotation.Keep;
+
+import com.google.firebase.database.Exclude;
+
+public class ModelCrypto {
+
+
+    @Exclude private String name;
+    @Exclude private String symbol;
+    @Exclude private double price;
+
+
+    @Keep
+    public ModelCrypto(String name, String symbol, double price) {
+        this.name = name;
+        this.symbol = symbol;
+        this.price = price;
+    }
+
+    @Keep
+    public String getName() {
+        return name;
+    }
+
+    @Keep
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Keep
+    public String getSymbol() {
+        return symbol;
+    }
+
+    @Keep
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    @Keep
+    public double getPrice() {
+        return price;
+    }
+
+    @Keep
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
---
+++ app/src/main/java/com/example/cryptocurrencytracker/Model/ModelHl.java
@@ -0,0 +1,54 @@
+package com.example.cryptocurrencytracker.Model;
+
+import androidx.annotation.Keep;
+
+import com.google.firebase.database.Exclude;
+
+public class ModelHl {
+
+
+    @Exclude private String cId;
+    @Exclude private String ip;
+    @Exclude private String time;
+
+    @Keep
+    public ModelHl() {
+    }
+
+    @Keep
+    public ModelHl(String cId, String ip, String time) {
+        this.cId = cId;
+        this.ip = ip;
+        this.time = time;
+    }
+
+    @Keep
+    public String getcId() {
+        return cId;
+    }
+
+    @Keep
+    public void setcId(String cId) {
+        this.cId = cId;
+    }
+
+    @Keep
+    public String getIp() {
+        return ip;
+    }
+
+    @Keep
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    @Keep
+    public String getTime() {
+        return time;
+    }
+
+    @Keep
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
